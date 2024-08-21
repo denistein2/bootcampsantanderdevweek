2124847b52359fece2f9e3ac199684e1f8e20893
denistein2 <denisteinoficial@gmail.com>
2024-08-21 10:31:29 -0300

    Initial commit

---
+++ src/main/java/me/dio/bootcampsantanderdevweek/BootcampsantanderdevweekApplication.java
@@ -0,0 +1,13 @@
+package me.dio.bootcampsantanderdevweek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampsantanderdevweekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampsantanderdevweekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/bootcampsantanderdevweek/BootcampsantanderdevweekApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.bootcampsantanderdevweek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampsantanderdevweekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
